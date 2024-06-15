983d19f6c20325f193bb330de29ef49ffd8b9b68
Arpit7986 <arpitjain4746@gmail.com>
2024-06-15 12:15:46 +0530

    fs

---
+++ src/models/review.js
@@ -0,0 +1,17 @@
+const mongoose=require('mongoose')
+const reviewSchema = new mongoose.Schema({
+    rating:{
+        type:Number,
+        min:1,
+        max:5
+    },
+    comments:{
+        type:String,
+        trim:true
+    }
+},{
+    timestamps:true
+});
+
+const Review = mongoose.model('Review', reviewSchema);
+module.exports=Review
