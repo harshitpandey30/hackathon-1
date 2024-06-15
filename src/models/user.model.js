e32d35d41b3bbd3363a85a111682da1ee5b98470
Divyanshu-rajpoot <divy.r.007@gmail.com>
2024-06-15 11:55:25 +0530

    src added

---
+++ src/models/user.model.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "Please provide a name"],
+    },
+    email: {
+        type: String,
+        required: [true, "Please provide an email"],
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: [true, "Please provide a password"],
+    },
+});
+
+export const User = mongoose.model("User", userSchema);
